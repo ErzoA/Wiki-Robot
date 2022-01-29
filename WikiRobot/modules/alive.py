9d167a7e62565850c7c19d800f2d293ebead71b0
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-29 11:49:43 +0800

    Update alive.py

--- WikiRobot/modules/alive.py
+++ WikiRobot/modules/alive.py
@@ -9,16 +9,16 @@ from WikiRobot.events import register
 from WikiRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/9b9a27cd02e65046d5515.jpg"
+PHOTO = "https://telegra.ph/file/b908dc3669562895e8afa.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
   TEXT += "⚪ **Saya Bekerja Dengan Benar** \n\n"
-  TEXT += f"⚪ **Tuanku : [Wiki W](WikiTapiOrang)** \n\n"
+  TEXT += f"⚪ **My Darling : [Wiki W](@Hanya_W)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Terima Kasih Telah Menambahkan Saya Di Sini ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/WikiTapiBot?start=help"), Button.url("Support", "https://t.me/WikiTapiGroup")]]
+  BUTTON = [[Button.url("Help", "https://t.me/MuscatXBot?start=help"), Button.url("Support", "https://t.me/WikiTapiGroup")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)

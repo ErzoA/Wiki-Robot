e374f186182e32b55b9b598ed6dbafe4e8b31126
ErzoA <96314386+ErzoA@users.noreply.github.com>
2023-02-27 18:53:07 +0800

    Update mongo.py

--- WikiRobot/mongo.py
+++ WikiRobot/mongo.py
@@ -12,14 +12,14 @@ from EmikoRobot.confing import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "EmikoRobot"
+MONGO_DB = "WikiRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["emiexrobot"]
+db = client["WikiTapiBot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
